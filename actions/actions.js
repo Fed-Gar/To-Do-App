aa40a8bdbaf9a37c8cdc81b686c13e35c2371ea6
Fede García <garcia.fed3rico@gmail.com>
2021-08-23 11:01:31 -0300

    Delete actions directory

--- actions/actions.js
+++
@@ -1,5 +0,0 @@
-export const ADD_TODO = 'ADD_TODO';
-export const REMOVE_TODO = 'REMOVE_TODO'
-export const TO_DO = 'TO_DO'
-export const TO_IN_PROGRESS = 'TO_IN_PROGRESS';
-export const TO_DONE = 'TO_DONE';
